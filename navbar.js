ebf5d11fcf0383827963729e622544ac9a5c456a
krzychulus <43789645+krzychulus@users.noreply.github.com>
2018-11-26 17:18:01 +0100

    Kol1

---
+++ navbar.js
@@ -0,0 +1,7 @@
+let nav= '<nav><a href="index.html"> Index</a>'+
+            '<a href="kontakt.html">Kontakt  </a></nav>';
+
+
+    let navBar = document.querySelector('.navbar');
+
+    navBar.innerHTML = nav;
